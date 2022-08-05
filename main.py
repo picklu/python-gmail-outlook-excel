b8509fd4edefbd1d01fad5144e2159181907e148
Subrata Sarker <subrata_sarker@yahoo.com>
2022-08-05 13:18:33 +0600

    Print message for mail not sent to

--- main.py
+++ main.py
@@ -56,8 +56,7 @@ def send_gmail(mail, student):
     msg = Message(**mail_property)
     mail.send(msg)
 
-    print(
-        f"==> Mail sent to {student.name}({student.id}) of {student.department} successfully!")
+    print(f"==> Mail sent to {student.name}<{student.email}> [{student.id}]")
 
 
 if __name__ == "__main__":
@@ -86,12 +85,16 @@ if __name__ == "__main__":
                     case "mobile":
                         student.mobile = cell.value
                     case "paid":
-                        student.paid = cell.value
+                        student.paid = cell.value == "Paid"
                     case "file name":
                         student.file_path = os.path.join(
                             dt_folder, cell.value)
 
             if student.name and student.paid:
-                # print(student.name, student.id, student.email)
+                print(
+                    f"==> Mail to be sent to {student.name}<{student.email}> [{student.id}]")
                 send_gmail(mail, student)
+            else:
+                print(
+                    f"==O Mail not to be sent to {student.name}<{student.email}> [{student.id}]")
     print("==> Done!")
